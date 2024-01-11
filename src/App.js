f4e4a1c8f2baa0fb46badd864d2e4f41366458e8
anupama <anupamaunni612@gmail.com>
2024-01-11 14:21:34 +0530

    craterd a list item in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className='header-title'>code malayalam</span>
+        <span>Home</span>
+        <span>Usage</span>
+        <span>Settings</span>
+        <span>Logout</span>
       </header>
+      <div className='app-body'>      
+          <div className='app-list'>
+            <div className='list-item'>
+              <hr/>
+              <div className='list-title'>
+                This is very big description
+              </div>
+              <div className='list-label'>
+                <span>Label 1</span>
+                <span>Label 2</span>
+                <span>Label 3</span>                
+              </div>
+              <hr/>
+            </div> 
+
+            <div className='list-item'>
+              <hr/>
+              <div className='list-title'>
+                This is very big description
+              </div>
+              <div className='list-label'>
+                <span>Label 1</span>
+                <span>Label 2</span>
+                <span>Label 3</span>                
+              </div>
+              <hr/>
+            </div> 
+
+            <div className='list-item'>
+              <hr/>
+              <div className='list-title'>
+                This is very big description
+              </div>
+              <div className='list-label'>
+                <span>Label 1</span>
+                <span>Label 2</span>
+                <span>Label 3</span>                
+              </div>
+              <hr/>
+            </div> 
+
+          </div>        
+      </div>
+      <div className='app-footer'>
+          Copyright codemalayalm.in All Right Reserved
+      </div>
     </div>
   );
 }
